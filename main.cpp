497b3a987fb5666f88c1561655aa57ca0e9b84dc
Jonathan Kron <schule.jonathank@gmail.com>
2024-11-08 14:58:59 +0100

    joa ne

--- main.cpp
+++ main.cpp
@@ -6,9 +6,32 @@
 #include <QAudioOutput>
 #include <QMediaDevices>
 
+#include <QDir>
+
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
+
+    QDir dir("/home/jonathankr/Github/nimamp/music");
+    dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+    dir.setSorting(QDir::Name | QDir::IgnoreCase);
+
+    QFileInfoList list = dir.entryInfoList();
+    foreach (QFileInfo file, list)
+    {
+        std::cout << qPrintable(file.fileName()) << std::endl;
+    }
+
     QGuiApplication app(argc, argv);
+
+    // QMediaPlayer player;
+    // QAudioOutput *audioOutput = new QAudioOutput();
+
+    // player.setAudioOutput(audioOutput);
+    // player.setSource(QUrl::fromLocalFile("/home/jonathankr/Github/nimamp/music/442.wav"));
+    // audioOutput->setVolume(0.5);
+    // player.play();
 
     QQmlApplicationEngine engine;
     QObject::connect(
