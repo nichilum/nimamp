8caa9613f9e139a395f6c62700953a267d506d47
Leo Kling <leo.kling.info@gmail.com>
2024-12-07 21:37:18 +0100

    remove dead code

--- headers/utils.hpp
+++ headers/utils.hpp
@@ -1,17 +1,5 @@
 #pragma once
 
-#include <taglib/apetag.h>
-#include <taglib/attachedpictureframe.h>
-#include <taglib/fileref.h>
-#include <taglib/id3v2tag.h>
-#include <taglib/mpegfile.h>
-#include <taglib/tag.h>
-
-#include <QImage>
-#include <QPixmap>
-#include <QVector>
-#include <algorithm>
-
 inline QString msToString(const qint64 position) {
     int seconds = static_cast<int>(position / 1000);
     int minutes = seconds / 60;
@@ -34,22 +22,3 @@ inline QString msToString(const qint64 position) {
 
     return formattedTime;
 }
-
-// inline QPixmap getSongThumbnail(const QString &filePath) {
-//     TagLib::MPEG::File file(filePath.toUtf8().data());
-//     if (file.ID3v2Tag()) {
-//         TagLib::ID3v2::Tag *tag = file.ID3v2Tag();
-//         TagLib::ID3v2::FrameList frames = tag->frameList("APIC");  // APIC is the frame for album art
-//         if (!frames.isEmpty()) {
-//             auto *pictureFrame = dynamic_cast<TagLib::ID3v2::AttachedPictureFrame *>(frames.front());
-//             if (pictureFrame) {
-//                 QByteArray imageData = QByteArray::fromRawData(pictureFrame->picture().data(), pictureFrame->picture().size());
-//                 QImage albumArt = QImage::fromData(imageData);
-//                 if (!albumArt.isNull()) {
-//                     // Display the album art or use it as needed
-//                     return QPixmap::fromImage(albumArt);
-//                 }
-//             }
-//         }
-//     }
-// }
