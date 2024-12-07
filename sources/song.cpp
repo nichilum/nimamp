6739947f2d4d2973767fe2a9db09e56b31cdda60
Leo Kling <leo.kling.info@gmail.com>
2024-12-07 21:31:20 +0100

    remove print

--- sources/song.cpp
+++ sources/song.cpp
@@ -8,8 +8,6 @@
 Song::Song(const QUrl &url) : url(url), filename(url.fileName()) {
     TagLib::MPEG::File file(url.toLocalFile().toUtf8().data());
 
-    qDebug() << url.toLocalFile();
-
     if (file.ID3v2Tag()) {
         TagLib::ID3v2::Tag *tag = file.ID3v2Tag();
         title = QString::fromStdString(tag->title().toCString(true));
