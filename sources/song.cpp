88e23e060ec88ede917b81c22998f658f17cc323
Leo Kling <leo.kling.info@gmail.com>
2024-12-07 21:11:33 +0100

    load some metadata

--- sources/song.cpp
+++ sources/song.cpp
@@ -1,1 +1,16 @@
 #include "../headers/song.hpp"
+
+#include <taglib/id3v2tag.h>
+#include <taglib/mpegfile.h>
+
+Song::Song(const QUrl &url) : url(url), filename(url.fileName()) {
+    TagLib::MPEG::File file(url.toLocalFile().toUtf8().data());
+
+    qDebug() << url.toLocalFile();
+
+    if (file.ID3v2Tag()) {
+        TagLib::ID3v2::Tag *tag = file.ID3v2Tag();
+        title = QString::fromStdString(tag->title().toCString(true));
+        artist = QString::fromStdString(tag->artist().toCString(true));
+    }
+}
--- sources/song_item.cpp
+++ sources/song_item.cpp
@@ -10,14 +10,15 @@ SongItem::SongItem(const Song &song, SongItemType songItemType, QWidget *parent)
 SongItem::SongItem(const Song &song, SongItemType songItemType, int index, QWidget *parent)
     : QWidget(parent), ui(new Ui::SongItem) {
     ui->setupUi(this);
-    ui->songItemSongLabel->setText(song.getFilename());
+
+    ui->songItemSongLabel->setText(song.title == "" ? song.getFilename() : song.title);
+    ui->songItemArtistLabel->setText(song.artist == "" ? "Unknown artist" : song.artist);
     if (index >= 0) {
         ui->songItemNumber->setText(QString::number(index));
     } else {
         ui->songItemNumber->setText("");
     }
 
-    name = song.getFilename();
     auto player = Player::getInstance();
 
     // connect play button
